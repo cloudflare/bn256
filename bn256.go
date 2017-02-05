c948933dbf1b30a9e372b89a24ca1fb4183bfaa7
Brendan McMillion <brendan@cloudflare.com>
2017-02-05 10:13:19 -0800

    Interop tests with official bn256.

--- bn256.go
+++ bn256.go
@@ -92,7 +92,7 @@ func (e *G1) Set(a *G1) *G1 {
 	return e
 }
 
-// Marshal converts n to a byte slice.
+// Marshal converts e to a byte slice.
 func (e *G1) Marshal() []byte {
 	// Each value is a 256-bit number.
 	const numBytes = 256 / 8
@@ -215,7 +215,7 @@ func (e *G2) Set(a *G2) *G2 {
 	return e
 }
 
-// Marshal converts n into a byte slice.
+// Marshal converts e into a byte slice.
 func (e *G2) Marshal() []byte {
 	// Each value is a 256-bit number.
 	const numBytes = 256 / 8
@@ -276,4 +276,146 @@ func (e *G2) Unmarshal(m []byte) ([]byte, error) {
 	}
 
 	return m[4*numBytes:], nil
+}
+
+// GT is an abstract cyclic group. The zero value is suitable for use as the output of an operation, but cannot be used
+// as an input.
+type GT struct {
+	p *gfP12
+}
+
+// RandomGT returns x and e(g₁, g₂)ˣ where x is a random, non-zero number read from r.
+func RandomGT(r io.Reader) (*big.Int, *GT, error) {
+	k, err := randomK(r)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return k, new(GT).ScalarBaseMult(k), nil
+}
+
+func (g *GT) String() string {
+	return "bn256.GT" + g.p.String()
+}
+
+// ScalarBaseMult sets e to g*k where g is the generator of the group and then returns out.
+func (e *GT) ScalarBaseMult(k *big.Int) *GT {
+	if e.p == nil {
+		e.p = &gfP12{}
+	}
+	e.p.Exp(gfP12Gen, k)
+	return e
+}
+
+// ScalarMult sets e to a*k and then returns e.
+func (e *GT) ScalarMult(a *GT, k *big.Int) *GT {
+	if e.p == nil {
+		e.p = &gfP12{}
+	}
+	e.p.Exp(a.p, k)
+	return e
+}
+
+// Add sets e to a+b and then returns e.
+func (e *GT) Add(a, b *GT) *GT {
+	if e.p == nil {
+		e.p = &gfP12{}
+	}
+	e.p.Mul(a.p, b.p)
+	return e
+}
+
+// Neg sets e to -a and then returns e.
+func (e *GT) Neg(a *GT) *GT {
+	if e.p == nil {
+		e.p = &gfP12{}
+	}
+	e.p.Conjugate(a.p)
+	return e
+}
+
+// Set sets e to a and then returns e.
+func (e *GT) Set(a *GT) *GT {
+	if e.p == nil {
+		e.p = &gfP12{}
+	}
+	e.p.Set(a.p)
+	return e
+}
+
+// Marshal converts e into a byte slice.
+func (e *GT) Marshal() []byte {
+	// Each value is a 256-bit number.
+	const numBytes = 256 / 8
+
+	ret := make([]byte, numBytes*12)
+	temp := &gfP{}
+
+	montDecode(temp, &e.p.x.x.x)
+	temp.Marshal(ret)
+	montDecode(temp, &e.p.x.x.y)
+	temp.Marshal(ret[numBytes:])
+	montDecode(temp, &e.p.x.y.x)
+	temp.Marshal(ret[2*numBytes:])
+	montDecode(temp, &e.p.x.y.y)
+	temp.Marshal(ret[3*numBytes:])
+	montDecode(temp, &e.p.x.z.x)
+	temp.Marshal(ret[4*numBytes:])
+	montDecode(temp, &e.p.x.z.y)
+	temp.Marshal(ret[5*numBytes:])
+	montDecode(temp, &e.p.y.x.x)
+	temp.Marshal(ret[6*numBytes:])
+	montDecode(temp, &e.p.y.x.y)
+	temp.Marshal(ret[7*numBytes:])
+	montDecode(temp, &e.p.y.y.x)
+	temp.Marshal(ret[8*numBytes:])
+	montDecode(temp, &e.p.y.y.y)
+	temp.Marshal(ret[9*numBytes:])
+	montDecode(temp, &e.p.y.z.x)
+	temp.Marshal(ret[10*numBytes:])
+	montDecode(temp, &e.p.y.z.y)
+	temp.Marshal(ret[11*numBytes:])
+
+	return ret
+}
+
+// Unmarshal sets e to the result of converting the output of Marshal back into a group element and then returns e.
+func (e *GT) Unmarshal(m []byte) ([]byte, error) {
+	// Each value is a 256-bit number.
+	const numBytes = 256 / 8
+
+	if len(m) < 12*numBytes {
+		return nil, errors.New("bn256: not enough data")
+	}
+
+	if e.p == nil {
+		e.p = &gfP12{}
+	}
+
+	e.p.x.x.x.Unmarshal(m)
+	e.p.x.x.y.Unmarshal(m[numBytes:])
+	e.p.x.y.x.Unmarshal(m[2*numBytes:])
+	e.p.x.y.y.Unmarshal(m[3*numBytes:])
+	e.p.x.z.x.Unmarshal(m[4*numBytes:])
+	e.p.x.z.y.Unmarshal(m[5*numBytes:])
+	e.p.y.x.x.Unmarshal(m[6*numBytes:])
+	e.p.y.x.y.Unmarshal(m[7*numBytes:])
+	e.p.y.y.x.Unmarshal(m[8*numBytes:])
+	e.p.y.y.y.Unmarshal(m[9*numBytes:])
+	e.p.y.z.x.Unmarshal(m[10*numBytes:])
+	e.p.y.z.y.Unmarshal(m[11*numBytes:])
+	montEncode(&e.p.x.x.x, &e.p.x.x.x)
+	montEncode(&e.p.x.x.y, &e.p.x.x.y)
+	montEncode(&e.p.x.y.x, &e.p.x.y.x)
+	montEncode(&e.p.x.y.y, &e.p.x.y.y)
+	montEncode(&e.p.x.z.x, &e.p.x.z.x)
+	montEncode(&e.p.x.z.y, &e.p.x.z.y)
+	montEncode(&e.p.y.x.x, &e.p.y.x.x)
+	montEncode(&e.p.y.x.y, &e.p.y.x.y)
+	montEncode(&e.p.y.y.x, &e.p.y.y.x)
+	montEncode(&e.p.y.y.y, &e.p.y.y.y)
+	montEncode(&e.p.y.z.x, &e.p.y.z.x)
+	montEncode(&e.p.y.z.y, &e.p.y.z.y)
+
+	return m[12*numBytes:], nil
 }
--- bn256_test.go
+++ bn256_test.go
@@ -3,159 +3,115 @@ package bn256
 import (
 	"testing"
 
+	"bytes"
 	"crypto/rand"
-	"fmt"
-	"math/big"
+
+	"golang.org/x/crypto/bn256"
 )
 
-func TestGFPAdd(t *testing.T) {
-	for i := 0; i < 10000; i++ {
-		A, _ := rand.Int(rand.Reader, p)
-		B, _ := rand.Int(rand.Reader, p)
-		AB, BB := A.Bits(), B.Bits()
-
-		a := &gfP{uint64(AB[0]), uint64(AB[1]), uint64(AB[2]), uint64(AB[3])}
-		b := &gfP{uint64(BB[0]), uint64(BB[1]), uint64(BB[2]), uint64(BB[3])}
-		c := &gfP{}
-
-		gfpAdd(c, a, b)
-		C := new(big.Int).Add(A, B)
-		C.Mod(C, p)
-
-		if fmt.Sprintf("%64.64x", C) != fmt.Sprint(c) {
-			t.Logf("%64.64x", C)
-			t.Log(c)
-			t.Fatal()
-		}
+func TestG1(t *testing.T) {
+	k, Ga, err := RandomG1(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
 	}
-}
+	ma := Ga.Marshal()
 
-func TestGFPSub(t *testing.T) {
-	for i := 0; i < 10000; i++ {
-		A, _ := rand.Int(rand.Reader, p)
-		B, _ := rand.Int(rand.Reader, p)
-		AB, BB := A.Bits(), B.Bits()
+	Gb := new(bn256.G1).ScalarBaseMult(k)
+	mb := Gb.Marshal()
 
-		a := &gfP{uint64(AB[0]), uint64(AB[1]), uint64(AB[2]), uint64(AB[3])}
-		b := &gfP{uint64(BB[0]), uint64(BB[1]), uint64(BB[2]), uint64(BB[3])}
-		c := &gfP{}
-
-		gfpSub(c, a, b)
-		C := new(big.Int).Sub(A, B)
-		C.Mod(C, p)
-
-		if fmt.Sprintf("%64.64x", C) != fmt.Sprint(c) {
-			t.Logf("%64.64x", C)
-			t.Log(c)
-			t.Fatal()
-		}
-	}
-}
-
-func TestGFPMul(t *testing.T) {
-	for i := 0; i < 10000; i++ {
-		A, _ := rand.Int(rand.Reader, p)
-		B, _ := rand.Int(rand.Reader, p)
-		A.Lsh(A, 256).Mod(A, p)
-		AB := A.Bits()
-
-		C := new(big.Int).Mul(A, B)
-		C.Mod(C, p)
-		CB := C.Bits()
-
-		B.Lsh(B, 256).Mod(B, p)
-		BB := B.Bits()
-
-		a := &gfP{uint64(AB[0]), uint64(AB[1]), uint64(AB[2]), uint64(AB[3])}
-		b := &gfP{uint64(BB[0]), uint64(BB[1]), uint64(BB[2]), uint64(BB[3])}
-		c := &gfP{}
-
-		gfpMul(c, a, b)
-
-		if fmt.Sprintf("%64.64x", C) != fmt.Sprint(c) {
-			t.Logf("%64.64x", CB)
-			t.Log(c)
-			t.Fatal()
-		}
-	}
-}
-
-func TestOrderG1(t *testing.T) {
-	g := new(G1).ScalarBaseMult(Order)
-	if !g.p.IsInfinity() {
-		t.Error("G1 has incorrect order")
-	}
-
-	one := new(G1).ScalarBaseMult(new(big.Int).SetInt64(1))
-	g.Add(g, one)
-	g.p.MakeAffine()
-	if g.p.x != one.p.x || g.p.y != one.p.y {
-		t.Errorf("1+0 != 1 in G1")
+	if !bytes.Equal(ma, mb) {
+		t.Fatal("bytes are different")
 	}
 }
 
 func TestG1Marshal(t *testing.T) {
-	g, g2 := new(G1).ScalarBaseMult(new(big.Int).SetInt64(1)), new(G1)
-	form := g.Marshal()
-	rest, err := new(G1).Unmarshal(form)
-	if err != nil || len(rest) > 0 {
-		t.Fatalf("failed to unmarshal: %v", err)
+	_, Ga, err := RandomG1(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
 	}
+	ma := Ga.Marshal()
 
-	g.ScalarBaseMult(Order)
-	form = g.Marshal()
-	rest, err = g2.Unmarshal(form)
-	if err != nil || len(rest) > 0 {
-		t.Fatalf("failed to unmarshal ∞: %v", err)
-	} else if !g2.p.IsInfinity() {
-		t.Fatalf("∞ unmarshaled incorrectly")
+	Gb := new(G1)
+	_, err = Gb.Unmarshal(ma)
+	if err != nil {
+		t.Fatal(err)
 	}
-}
+	mb := Gb.Marshal()
 
-func TestG1Identity(t *testing.T) {
-	g := new(G1).ScalarBaseMult(new(big.Int).SetInt64(0))
-	if !g.p.IsInfinity() {
-		t.Error("failure")
+	if !bytes.Equal(ma, mb) {
+		t.Fatal("bytes are different")
 	}
 }
 
-func TestOrderG2(t *testing.T) {
-	g := new(G2).ScalarBaseMult(Order)
-	if !g.p.IsInfinity() {
-		t.Error("G2 has incorrect order")
+func TestG2(t *testing.T) {
+	k, Ga, err := RandomG2(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
 	}
+	ma := Ga.Marshal()
 
-	one := new(G2).ScalarBaseMult(new(big.Int).SetInt64(1))
-	g.Add(g, one)
-	g.p.MakeAffine()
+	Gb := new(bn256.G2).ScalarBaseMult(k)
+	mb := Gb.Marshal()
 
-	if *g.p != *twistGen {
-		t.Errorf("1+0 != 1 in G2")
+	if !bytes.Equal(ma, mb) {
+		t.Fatal("bytes are different")
 	}
 }
 
 func TestG2Marshal(t *testing.T) {
-	g, g2 := new(G2).ScalarBaseMult(new(big.Int).SetInt64(1)), new(G2)
-	form := g.Marshal()
-	rest, err := new(G2).Unmarshal(form)
-	if err != nil || len(rest) > 0 {
-		t.Fatalf("failed to unmarshal: %v", err)
+	_, Ga, err := RandomG2(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
 	}
+	ma := Ga.Marshal()
 
-	g.ScalarBaseMult(Order)
-	form = g.Marshal()
-	rest, err = g2.Unmarshal(form)
-	if err != nil || len(rest) > 0 {
-		t.Fatalf("failed to unmarshal ∞: %v", err)
-	} else if !g2.p.IsInfinity() {
-		t.Fatalf("∞ unmarshaled incorrectly")
+	Gb := new(G2)
+	_, err = Gb.Unmarshal(ma)
+	if err != nil {
+		t.Fatal(err)
+	}
+	mb := Gb.Marshal()
+
+	if !bytes.Equal(ma, mb) {
+		t.Fatal("bytes are different")
 	}
 }
 
-func TestG2Identity(t *testing.T) {
-	g := new(G2).ScalarBaseMult(new(big.Int).SetInt64(0))
-	if !g.p.IsInfinity() {
-		t.Error("failure")
+func TestGT(t *testing.T) {
+	k, Ga, err := RandomGT(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ma := Ga.Marshal()
+
+	Gb, ok := new(bn256.GT).Unmarshal((&GT{gfP12Gen}).Marshal())
+	if !ok {
+		t.Fatal("unmarshal not ok")
+	}
+	Gb.ScalarMult(Gb, k)
+	mb := Gb.Marshal()
+
+	if !bytes.Equal(ma, mb) {
+		t.Fatal("bytes are different")
+	}
+}
+
+func TestGTMarshal(t *testing.T) {
+	_, Ga, err := RandomGT(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ma := Ga.Marshal()
+
+	Gb := new(GT)
+	_, err = Gb.Unmarshal(ma)
+	if err != nil {
+		t.Fatal(err)
+	}
+	mb := Gb.Marshal()
+
+	if !bytes.Equal(ma, mb) {
+		t.Fatal("bytes are different")
 	}
 }
 
@@ -174,5 +130,14 @@ func BenchmarkG2(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		new(G2).ScalarBaseMult(x)
+	}
+}
+
+func BenchmarkGT(b *testing.B) {
+	x, _ := rand.Int(rand.Reader, Order)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		new(GT).ScalarBaseMult(x)
 	}
 }
